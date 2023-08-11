3e0b5da65f7158b739b0be2f4868a3924ee145a9
Rich Wincott <rwincott@fosterandpartners.com>
2023-08-11 09:21:20 +0100

    node server

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const app = express()
 const port = 3100;
 const cors = require('cors')
 const yt = require('youtube-search-without-api-key');
-const { query } = require('express');
+const axios = require('axios');
+const https = require('https')
 
 app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json())
 
 app.get('/youtubeSearch', async (req, res) => {
   const query = req.query.query;
-  if (!query) return res.json({ message: 'Query value not provided' })
+  if (!query) return res.json({ message: 'Please provide the querystring value: query' })
   const videos = await yt.search(query);
   res.json(videos)
+})
+
+app.get('/youtubeMeta', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json({ message: 'Please provide the querystring value: url' })
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+  try {
+    const data = await axios.get('https://dev.richardwincott.co.uk/youtubemetadata?url=' + url, { httpsAgent: agent })
+    res.json(data)
+  }
+  catch (err) {
+    res.json({ message: err.message })
+  }
 })
 
 app.listen(port, () => {
